b01e18e2051b1194f09372dfd0b10a608981d0ea
zen <zhangyiming748@gmail.com>
2024-05-02 18:38:51 +0800

    设置默认值

---
+++ constant/constant.go
@@ -0,0 +1,34 @@
+package constant
+
+var (
+	root     string = "/data"
+	language string = "en"
+	pattern  string = "mp4"
+	model    string = "/root/app"
+)
+
+func GetRoot() string {
+	return root
+}
+
+func GetLanguage() string {
+	return language
+}
+func GetPattern() string {
+	return pattern
+}
+func GetModel() string {
+	return model
+}
+func SetRoot(s string) {
+	root = s
+}
+func SetLanguage(s string) {
+	language = s
+}
+func SetPattern(s string) {
+	pattern = s
+}
+func SetModel(s string) {
+	model = s
+}
